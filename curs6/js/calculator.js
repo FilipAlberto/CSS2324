0d6bc0dd5d36a4b7f78b8266656fa16a698d5fb6
Frontend JavaScript <frontendjavascript@iMac-24.local>
2024-03-06 20:43:23 +0200

    js2324

---
+++ curs6/js/calculator.js
@@ -0,0 +1,11 @@
+import{add} from './ap1_1.js';
+import{dif} from './ap1_1.js';
+import{inm} from './ap1_1.js';
+
+
+let x = 6;
+let y = 4;
+
+console.info( add(x,y));
+console.info( dif(x,y));
+console.info( inm(x,y));
